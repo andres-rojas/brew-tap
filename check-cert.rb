c3eabb57a12bad01cf327d48c4cf3d76484fe9c0
Andres Rojas <andres@conpat.io>
2018-08-06 16:14:05 -0700

    Update URL of check-cert

--- check-cert.rb
+++ check-cert.rb
@@ -1,7 +1,7 @@
 class CheckCert < Formula
   desc "A command line tool to retrieve the details of a remote SSL certificate"
-  homepage "https://github.com/andres-rojas/cli-tools/tree/master/misc"
-  url "https://github.com/andres-rojas/cli-tools/raw/master/misc/check-cert"
+  homepage "https://github.com/andres-rojas/cli-tools/tree/master/ssl"
+  url "https://github.com/andres-rojas/cli-tools/raw/master/ssl/check-cert"
   sha256 "56ae383924e236e89dc60360f73403df2f2aeb9f4f0c2400f92f6ea097d9e5ac"
   version "1.0.1"
 
