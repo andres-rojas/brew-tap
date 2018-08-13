fd1da48b2c73d605fc96929013efe2ac43b5d2cb
Andres Rojas <andres@conpat.io>
2018-08-13 16:35:54 -0700

    Update ssh-ec2

--- ssh-ec2.rb
+++ ssh-ec2.rb
@@ -2,8 +2,8 @@ class SshEc2 < Formula
   desc "A command line tool to ssh in to EC2 instances by their 'Name' tag"
   homepage "https://github.com/andres-rojas/cli-tools/tree/master/aws"
   url "https://github.com/andres-rojas/cli-tools/raw/master/aws/ssh-ec2"
-  sha256 "a43dd91cec1f8de410f50d387170aa27ebd27f4092cb626fa9fc80b937449e3f"
-  version "1.1.0"
+  sha256 "d18cbefcc1386ced73a0c2647b10ebdd28a4b593e27c7a1721384d62cbbcbf36"
+  version "1.2.0"
 
   depends_on "awscli"
   depends_on "jq"
