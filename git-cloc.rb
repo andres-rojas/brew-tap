f1e7a31262e5acd414ac05b33ea4d1f629422077
Andres Rojas <andres@conpat.io>
2018-08-14 15:52:52 -0700

    Update git-cloc

--- git-cloc.rb
+++ git-cloc.rb
@@ -1,13 +1,14 @@
 class GitCloc < Formula
-  desc "A command line tool to run cloc on a git repo"
-  homepage "https://github.com/andres-rojas/cli-tools/tree/master/misc"
-  url "https://github.com/andres-rojas/cli-tools/raw/master/misc/git-cloc"
-  sha256 "351238baf510769a994acb8b5614aa782fe2fe92b2cee0bdfc0636645bad9cc5"
-  version "1.0.1"
+  desc 'A command line tool to run cloc on a git repo'
+  homepage 'https://github.com/andres-rojas/cli-tools/tree/master/misc'
+  url 'https://github.com/andres-rojas/cli-tools/raw/master/misc/git-cloc'
+  sha256 'e6bbfa6f6b0b59f27dbcbf298e2fb7238953bb856dda7343b5658e36d98afbad'
+  version '1.1.0'
 
-  depends_on "cloc"
+  depends_on 'cloc'
+  depends_on 'git'
 
   def install
-    bin.install "git-cloc"
+    bin.install 'git-cloc'
   end
 end
