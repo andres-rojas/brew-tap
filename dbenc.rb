2d3fd81ae66114b2ed763b47492730deeae0117a
Andres Rojas <andres@conpat.io>
2018-08-13 18:01:31 -0700

    Update dbenc

--- dbenc.rb
+++ dbenc.rb
@@ -1,11 +1,11 @@
 class Dbenc < Formula
-  desc "A command line tool to toggle automatic data bag encryption for your local Chef environment"
-  homepage "https://github.com/andres-rojas/cli-tools/tree/master/chef"
-  url "https://github.com/andres-rojas/cli-tools/raw/master/chef/dbenc"
-  sha256 "fdc339b33a2898023725a39d04e5c07421b11a2759725111e3f7d4de7d3000fa"
-  version "2.1.0"
+  desc 'A command line tool to toggle automatic data bag encryption for your local Chef environment'
+  homepage 'https://github.com/andres-rojas/cli-tools/tree/master/chef'
+  url 'https://github.com/andres-rojas/cli-tools/raw/master/chef/dbenc'
+  sha256 '33f5a1384694b81b9ba00abd00033120cdc55f1596d6f7c41d1d7261057b8935'
+  version '2.2.0'
 
   def install
-    bin.install "dbenc"
+    bin.install 'dbenc'
   end
 end
