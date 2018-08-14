f27b50e83dd627f8bf851e723ccdc6a3f35c5ce7
Andres Rojas <andres@conpat.io>
2018-08-14 10:41:43 -0700

    Update vendor-cookbooks

--- vendor-cookbooks.rb
+++ vendor-cookbooks.rb
@@ -1,11 +1,11 @@
 class VendorCookbooks < Formula
-  desc "A command line tool to package Berkshelf-vendorized Chef cookbooks into a standardized chef-repo tarball"
-  homepage "https://github.com/andres-rojas/cli-tools/tree/master/chef"
-  url "https://github.com/andres-rojas/cli-tools/raw/master/chef/vendor-cookbooks"
-  sha256 "118059e8327ca87f8384bb36a617b634b4d8965b0ba1cd39247296de34e4bd72"
-  version "1.0.0"
+  desc 'A command line tool to package Berkshelf-vendorized Chef cookbooks into a standardized chef-repo tarball'
+  homepage 'https://github.com/andres-rojas/cli-tools/tree/master/chef'
+  url 'https://github.com/andres-rojas/cli-tools/raw/master/chef/vendor-cookbooks'
+  sha256 'd68984070b5f468a10b945ae0a36448a67cf80c061a5b4c2a2eed8535e4d0307'
+  version '1.1.0'
 
   def install
-    bin.install "vendor-cookbooks"
+    bin.install 'vendor-cookbooks'
   end
 end
