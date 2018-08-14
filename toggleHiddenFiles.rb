d4e5aacaa5849ae4f3cb900a86606176147fc316
Andres Rojas <andres@conpat.io>
2018-08-14 14:40:53 -0700

    Update toggleHiddenFiles

--- toggleHiddenFiles.rb
+++ toggleHiddenFiles.rb
@@ -2,8 +2,8 @@ class Togglehiddenfiles < Formula
   desc 'A command line tool to toggle the visibility of hidden files in macOS Finder'
   homepage 'https://github.com/andres-rojas/cli-tools/tree/master/macOS'
   url 'https://github.com/andres-rojas/cli-tools/raw/master/macOS/toggleHiddenFiles'
-  sha256 '25bca245409f0cc63ba9f9dfd65f18e6c73582dffc39d6dfb845f3decd54a337'
-  version '1.1.0'
+  sha256 '51aa59c1622abc345fad491e3577d8edbe1e8d37b6d6598b4f5e3f6f94d8dbc9'
+  version '1.1.1'
 
   def install
     bin.install 'toggleHiddenFiles'
