069dea6c8fcac2ed8b73835493462d873135ca1a
Andres Rojas <andres@conpat.io>
2018-08-14 11:55:24 -0700

    Update bootiso2usb

--- bootiso2usb.rb
+++ bootiso2usb.rb
@@ -1,11 +1,11 @@
 class Bootiso2usb < Formula
-  desc "A command line tool to write a bootable ISO to a USB device on macOS"
-  homepage "https://github.com/andres-rojas/cli-tools/tree/master/macOS"
-  url "https://github.com/andres-rojas/cli-tools/raw/master/macOS/bootiso2usb"
-  sha256 "1a93e0def7be8b290531832f54db2a9835ad52fa6b2a91da84dc93402eff2f28"
-  version "1.0.0"
+  desc 'A command line tool to write a bootable ISO to a USB device on macOS'
+  homepage 'https://github.com/andres-rojas/cli-tools/tree/master/macOS'
+  url 'https://github.com/andres-rojas/cli-tools/raw/master/macOS/bootiso2usb'
+  sha256 'ada37f92e402ddc3422c79392160f68eccd034dbf8ab1fa060d038aa9ca8f22a'
+  version '1.1.0'
 
   def install
-    bin.install "bootiso2usb"
+    bin.install 'bootiso2usb'
   end
 end
