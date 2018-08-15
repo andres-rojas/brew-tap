e2b2ffc97412bdce9f9a2ecb3f7dce17646d0820
Andres Rojas <andres@conpat.io>
2018-08-14 17:50:11 -0700

    Update sshwa

--- sshwa.rb
+++ sshwa.rb
@@ -2,8 +2,8 @@ class Sshwa < Formula
   desc 'A command line tool to wait for a successful port knock before SSHing in to a host'
   homepage 'https://github.com/andres-rojas/cli-tools/tree/master/misc'
   url 'https://github.com/andres-rojas/cli-tools/raw/master/misc/sshwa'
-  sha256 '53d2ff6a3755a38fc797beca526496aedb491fa6c9d2ff48b536d70b90fbe131'
-  version '1.1.0'
+  sha256 '699162ab846b6228f6c6957fe4f27f8722608247e7782580f68f3f67a3ae0707'
+  version '1.2.0'
 
   def install
     bin.install 'sshwa'
