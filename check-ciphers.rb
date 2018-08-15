b0b755eb5aaaf9e98b4d0b5efab0e69f0b64f168
Andres Rojas <andres@conpat.io>
2018-08-15 01:43:46 -0700

    Update check-ciphers

--- check-ciphers.rb
+++ check-ciphers.rb
@@ -2,7 +2,7 @@ class CheckCiphers < Formula
   desc 'A command line tool to audit the valid ciphers for a host'
   homepage 'https://github.com/andres-rojas/cli-tools/tree/master/ssl'
   url 'https://github.com/andres-rojas/cli-tools/raw/master/ssl/check-ciphers'
-  sha256 '6f10a29ba6cf014dd885ca1df9ee49741bb7f71695ed254eff2cc0f733230a15'
+  sha256 '4bedafc63c9e9226f78bc8d789ce2541aa8718ecbad95e30275beaad43633a88'
   version '1.1.0'
 
   depends_on 'openssl' => :optional # Need to be careful about blanket updating openssl
