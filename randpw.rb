ad24ac2a57b120ce312fae3200230d47ddc8b8d3
Andres Rojas <andres@conpat.io>
2018-08-15 02:05:21 -0700

    Update randpw

--- randpw.rb
+++ randpw.rb
@@ -2,8 +2,8 @@ class Randpw < Formula
   desc 'A commmand line tool to generate a random alphanumeric password'
   homepage 'https://github.com/andres-rojas/cli-tools/tree/master/misc'
   url 'https://github.com/andres-rojas/cli-tools/raw/master/misc/randpw'
-  sha256 'f4b1d78bcbed068d28d9a0eef74df7040ffc7f9800ef8f886042e43aa464b7e6'
-  version '1.1.0'
+  sha256 '7400978ea530d1042b9ccad9508e52c31df861c084cceeee85ca8eca554f5747'
+  version '1.1.1'
 
   def install
     bin.install 'randpw'
