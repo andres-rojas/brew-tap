e333f2cd1b5351ea607c71bb36230b62c3bd5531
Andres Rojas <andres@conpat.io>
2018-11-05 18:41:31 -0800

    Update toggleDarkSlack

--- toggleDarkSlack.rb
+++ toggleDarkSlack.rb
@@ -2,8 +2,8 @@ class Toggledarkslack < Formula
   desc 'A command line tool to toggle a custom Dark mode theme for the macOS Slack app'
   homepage 'https://github.com/andres-rojas/cli-tools/tree/master/macOS'
   url 'https://github.com/andres-rojas/cli-tools/raw/master/macOS/toggleDarkSlack'
-  sha256 'ae5ec01544d4fef35338009ce0e9b826f4a9493b8f1f2962c974fbc63d717ed1'
-  version '1.0.0'
+  sha256 '311f55a9395693dfaf154f2c5aa673cf85b7141e4cca4c23ebf5eba7df6a5214'
+  version '1.0.1'
 
   def install
     bin.install 'toggleDarkSlack'
